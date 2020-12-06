256722b6ab45a0952a39e14e6df41f165e437eaa
Trevor DeGruccio <trevordegruccio97@yahoo.com>
2020-12-05 19:38:13 -0500

    Add files via upload
    Code is from https://github.com/perrygeo/simanneal/tree/951e7d89a8b7f19aeb05b64e7cc8b844a734af89
    
    Going to modify to have GUI and parallelize it!

---
+++ simanneal/__init__.py
@@ -0,0 +1,6 @@
+# CREDIT TO: https://github.com/perrygeo/simanneal/tree/951e7d89a8b7f19aeb05b64e7cc8b844a734af89
+from __future__ import absolute_import
+from .anneal import Annealer
+
+__all__ = ['Annealer']
+__version__ = "0.5.0"
